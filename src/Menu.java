e1b1bd9892ee4bf1cbb0c27820db78c767e68f25
Ryan-Luka <ryanlukao@gmail.com>
2025-08-27 20:10:26 -0300

    primeiro commit

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,59 @@
-package PACKAGE_NAME;
-
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+import java.util.Random;
 public class Menu {
+    public static void main(String[] args) {
+        ArrayList<String> titulo = new ArrayList<>();
+        ArrayList<String> autor = new ArrayList<>();
+        ArrayList<String> ano = new ArrayList<>();
+        ArrayList<Integer> codigo = new ArrayList<>();
+        Random rand = new Random();
+        while(true){
+            int opcao = Integer.parseInt(JOptionPane.showInputDialog(
+                    """
+                    Menu Principal:
+                    1 - Cadastrar novo livro
+                    2 - Listar todos os livros
+                    3 - Buscar livro por título
+                    4 - Realizar empréstimo de livro
+                    5 - Registrar devolução de livro
+                    6 - Excluir livro do sistema
+                    7 - Listar livros emprestados
+                    8 - Contar livros disponíveis e emprestados
+                    9 - Sair
+                    """));
+
+            switch (opcao){
+                case 1:
+                    String tituloLivro = JOptionPane.showInputDialog("Informe o título do livro:");
+                    titulo.add(tituloLivro);
+
+                    String autorLivro = JOptionPane.showInputDialog("Informe autor do livro:");
+                    autor.add(autorLivro);
+
+                    String anoLivro = JOptionPane.showInputDialog("Informe a ano do livro:");
+                    ano.add(anoLivro);
+
+                    int codigoLivro;
+                    do {
+                        codigoLivro = rand.nextInt(1000) + 1;
+                    } while (codigo.contains(codigoLivro));
+
+                    codigo.add(codigoLivro);
+
+                    break;
+
+
+                case 2:
+                    for (int i = 0; i <= titulo.size(); i++) {
+                        JOptionPane.showMessageDialog(null, "Livros: \n" +
+                                "Título: " + titulo.get(i) + ", Autor: " + autor.get(i) + ", Ano: " + ano.get(i) +
+                                ", Código: " +  codigo.get(i) + ", Status: ");
+                    }
+                    break;
+            }
+        }
+
+    }
 }
+
