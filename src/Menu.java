68bffcbf3c3f6b6434bf784f5912e06a04bfbf21
Ryan-Luka <ryanlukao@gmail.com>
2025-08-28 23:16:29 -0300

    case 6, 7, 8

--- src/Menu.java
+++ src/Menu.java
@@ -1,15 +1,16 @@
 import javax.swing.JOptionPane;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Random;
 public class Menu {
     public static void main(String[] args) {
         ArrayList<String> titulo = new ArrayList<>();
         ArrayList<String> autor = new ArrayList<>();
         ArrayList<String> ano = new ArrayList<>();
         ArrayList<Integer> codigo = new ArrayList<>();
-        Random rand = new Random();
+        ArrayList<String> leitor = new ArrayList<>();
         HashMap<Integer, Boolean> disponibilidade = new HashMap<>();
+
+        int contadorCodigo = 0;
         while(true){
             int opcao = Integer.parseInt(JOptionPane.showInputDialog(
                     """
@@ -26,6 +27,8 @@ public class Menu {
                     """));
             String statusLivro;
             int codigoLivro;
+            int indice;
+            int codStatus;
             switch (opcao){
                 case 1:
                     String tituloLivro = JOptionPane.showInputDialog("Informe o título do livro:");
@@ -37,12 +40,12 @@ public class Menu {
                     String anoLivro = JOptionPane.showInputDialog("Informe a ano do livro:");
                     ano.add(anoLivro);
 
-                    do {
-                        codigoLivro = rand.nextInt(1000) + 1;
-                    } while (codigo.contains(codigoLivro));
+                    leitor.add("");
 
-                    codigo.add(codigoLivro);
-                    disponibilidade.put(codigoLivro, true);
+                    contadorCodigo++;
+                    codigo.add(contadorCodigo);
+
+                    disponibilidade.put(contadorCodigo, true);
 
                     break;
 
@@ -53,12 +56,8 @@ public class Menu {
                         livros.append("Nenhum livro cadastrado!");
                     }else {
                         for (int i = 0; i < titulo.size(); i++) {
-                            int codStatus = codigo.get(i);
-                            if (disponibilidade.get(codStatus)) {
-                                statusLivro = "Disponível";
-                            } else {
-                                statusLivro = "Emprestado";
-                            }
+                            codStatus = codigo.get(i);
+                            statusLivro = disponibilidade.get(codStatus) ? "Disponível" : "Emprestado";
                             livros.append("Título: ").append(titulo.get(i))
                                     .append(", Autor: ").append(autor.get(i))
                                     .append(", Ano: ").append(ano.get(i))
@@ -72,14 +71,9 @@ public class Menu {
                 case 3:
                     String buscarLivro = JOptionPane.showInputDialog("Informe o título do livro:");
                     if (titulo.contains(buscarLivro)){
-                        int indice = titulo.indexOf(buscarLivro);
-                        int codStatus = codigo.get(indice);
-                        if (disponibilidade.get(codStatus)){
-                            statusLivro = "Disponível";
-                        }
-                        else{
-                            statusLivro = "Emprestado";
-                        }
+                        indice = titulo.indexOf(buscarLivro);
+                        codStatus = codigo.get(indice);
+                        statusLivro = disponibilidade.get(codStatus) ? "Disponível" : "Emprestado";
                         JOptionPane.showMessageDialog(null, "Livros encontrados: \n" +
                                 "Título: " + titulo.get(indice) + ", Autor: " + autor.get(indice) +
                                 ", Ano: " + ano.get(indice) +
@@ -94,6 +88,9 @@ public class Menu {
                     if(disponibilidade.containsKey(codigoLivro)){
 
                         if (disponibilidade.get(codigoLivro)){
+                            String nomeLeitor = JOptionPane.showInputDialog("Informe seu nome:");
+                            indice = codigo.indexOf(codigoLivro);
+                            leitor.set(indice, nomeLeitor);
                             disponibilidade.put(codigoLivro, false);
                         }else{
                             JOptionPane.showMessageDialog(null, "Livro está emprestado!");
@@ -111,6 +108,8 @@ public class Menu {
                         if (disponibilidade.get(codigoLivro)){
                             JOptionPane.showMessageDialog(null, "Livro está disponível!");
                         }else{
+                            indice = codigo.indexOf(codigoLivro);
+                            leitor.set(indice, "");
                             disponibilidade.put(codigoLivro, true);
                         }
 
@@ -119,9 +118,81 @@ public class Menu {
                     }
                     break;
 
+                case 6:
+                    codigoLivro = Integer.parseInt(JOptionPane.showInputDialog("Informe o codigo do livro:"));
+                    if(codigo.contains(codigoLivro)){
+                        if(disponibilidade.get(codigoLivro)){
+                            Object[] opcoes = {"Sim", "Não", "Cancelar"};
+                            int resposta = JOptionPane.showInternalOptionDialog(null, "Escolha uma opção:", "Excluir livro?",
+                                    JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[2]);
+                            if (resposta == 0){
+                                indice = codigo.indexOf(codigoLivro);
+                                titulo.remove(indice);
+                                autor.remove(indice);
+                                ano.remove(indice);
+                                codigo.remove(indice);
+                                leitor.remove(indice);
+                                disponibilidade.remove(codigoLivro);
+                                JOptionPane.showMessageDialog(null, "Livro excluido com sucesso!");
+                            }else if (resposta == 1){
+                                JOptionPane.showMessageDialog(null, "Livro não excluido!");
+                            }else if (resposta == 2){
+                                JOptionPane.showMessageDialog(null, "Operação cancelada!");
+                            }
+                        }else{
+                            JOptionPane.showMessageDialog(null, "Livro está emprestado e não pode ser excluído!");
+                        }
+                    }else{
+                        JOptionPane.showMessageDialog(null, "Livro não encontrado!");
+                    }
+
+                    break;
+
+                case 7:
+                    StringBuilder livrosEmprestados = new StringBuilder("Livros Emprestados:\n");
+                    if (titulo.isEmpty()){
+                        livrosEmprestados.append("Nenhum livro cadastrado!");
+                    }else {
+                        for (int i = 0; i < titulo.size(); i++) {
+                            codStatus = codigo.get(i);
+                            statusLivro = disponibilidade.get(codStatus) ? "Disponível" : "Emprestado";
+                            if (!disponibilidade.get(codStatus)) {
+                                livrosEmprestados.append("Título: ").append(titulo.get(i))
+                                        .append(", Autor: ").append(autor.get(i))
+                                        .append(", Ano: ").append(ano.get(i))
+                                        .append(", Código: ").append(codigo.get(i))
+                                        .append(", Status: ").append(statusLivro)
+                                        .append(", Leitor: ").append(leitor.get(i)).append("\n");
+                            }
+                        }
+                    }
+                    JOptionPane.showMessageDialog(null, livrosEmprestados.toString());
+                    break;
+
+                case 8:
+                    int disponiveis = 0;
+                    int emprestados = 0;
+                    for (int i = 0; i < titulo.size(); i++) {
+                        codStatus = codigo.get(i);
+                        if (disponibilidade.get(codStatus)){
+                            disponiveis++;
+                        }else{
+                            emprestados++;
+                        }
+                    }
+                    int total = disponiveis + emprestados;
+                    JOptionPane.showMessageDialog(null, "Quantidade de livros disponíveis para emprestimo: " + disponiveis +
+                            "\nQuantidade de livros emprestados: " + emprestados +
+                            "\nQuantidade total: " + total);
+
+                    break;
+
+                case 9:
+                    System.exit(0);
+                    break;
+
             }
         }
-
     }
 }
 
